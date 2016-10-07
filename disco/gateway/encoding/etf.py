131ec9dda5e16f223251a0daf14924da5a9d3f18
Andrei <b1naryth1ef@gmail.com>
2016-10-06 23:26:13 -0500

    Delete unused function in gateway.encoding.etf

--- disco/gateway/encoding/etf.py
+++ disco/gateway/encoding/etf.py
@@ -1,18 +1,7 @@
-import six
-
 from websocket import ABNF
-from erlpack import Atom, unpack, pack
+from erlpack import unpack, pack
 
 from disco.gateway.encoding.base import BaseEncoder
-
-
-def make_keys_atom(obj):
-    res = {}
-    for k, v in six.iteritems(obj):
-        if isinstance(v, dict):
-            v = make_keys_atom(v)
-        res[Atom(k)] = v
-    return res
 
 
 class ETFEncoder(BaseEncoder):
