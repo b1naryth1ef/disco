1f50274554995500ba178c6b39ec35963a501fda
Andrei <b1naryth1ef@gmail.com>
2017-04-24 12:57:02 -0700

    Fix users loaded in GuildCreate missing presence information
    The presence data comes sep. from the users info here, so we need to
    load it.

--- disco/state.py
+++ disco/state.py
@@ -186,6 +186,9 @@ class State(object):
         for member in six.itervalues(event.guild.members):
             self.users[member.user.id] = member.user
 
+        for presence in event.presences:
+            self.users[presence.user.id].presence = presence
+
         for voice_state in six.itervalues(event.guild.voice_states):
             self.voice_states[voice_state.session_id] = voice_state
 
