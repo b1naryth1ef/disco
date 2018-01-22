ea6b882da71a637c99d86b70e67203879f0dab94
Andrei <b1naryth1ef@gmail.com>
2018-01-22 06:30:50 -0800

    [reqs] add wsaccel to performance reqs

--- setup.py
+++ setup.py
@@ -14,7 +14,11 @@ extras_require = {
     'http': ['flask==0.12.2'],
     'yaml': ['pyyaml==3.12'],
     'music': ['youtube_dl>=2018.1.14'],
-    'performance': ['erlpack==0.3.2' if sys.version_info.major == 2 else 'earl-etf==2.1.2', 'ujson==1.35'],
+    'performance': [
+        'erlpack==0.3.2' if sys.version_info.major == 2 else 'earl-etf==2.1.2',
+        'ujson==1.35',
+        'wsaccel==0.6.2',
+    ],
     'sharding': ['gipc==0.6.0'],
     'docs': ['biblio==0.0.4'],
 }
