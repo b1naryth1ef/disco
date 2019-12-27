bbddbc4956c6ee3046a38438db3b42b032f1921d
Luke <luke@lmbyrne.dev>
2019-12-27 23:30:47 +0000

    member_count tracking (#165)
    * member_count tracking
    
    * Remove unset checks.

--- disco/state.py
+++ disco/state.py
@@ -285,6 +285,10 @@ class State(object):
         if event.member.guild_id not in self.guilds:
             return
 
+        # Avoid adding duplicate events to member_count.
+        if event.member.id not in self.guilds[event.member.guild_id].members:
+            self.guilds[event.member.guild_id].member_count += 1
+
         self.guilds[event.member.guild_id].members[event.member.id] = event.member
 
     def on_guild_member_update(self, event):
@@ -302,6 +306,8 @@ class State(object):
 
         if event.user.id not in self.guilds[event.guild_id].members:
             return
+
+        self.guilds[event.guild_id].member_count -= 1
 
         del self.guilds[event.guild_id].members[event.user.id]
 
